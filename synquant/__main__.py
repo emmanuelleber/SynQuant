fcd9b4d7b67a7748167bf3365a792a2d29d9087d
BERTHAUX Emmanuelle <emmanuelle.berthaux@icm-institute.org>
2024-03-04 15:35:59 +0100

    add mean and std projections

--- synquant/__main__.py
+++ synquant/__main__.py
@@ -16,6 +16,14 @@ img = series[2].getFrame(channel=1, dtype=np.uint16)
 print(img.shape)
 
 # plot
-plt.imshow(np.std(img[:,:,:], axis=0), interpolation=None, cmap=plt.cm.binary)
+fig, AX = plt.subplots(1, 2)
+
+AX[0].set_title('mean')
+AX[0].imshow(np.mean(img[:,:,:], axis=0), interpolation=None, cmap=plt.cm.binary)
+AX[0].axis('off')
+
+AX[1].set_title('std')
+AX[1].imshow(np.std(img[:,:,:], axis=0), interpolation=None, cmap=plt.cm.binary)
+AX[1].axis('off')
 plt.show()
 
